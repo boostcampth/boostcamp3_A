cbe733dee43cb741b4a0951af8a7a23aa1464679
ch-Yoon <chanhyuck1021@naver.com>
2019-01-30 02:15:28 +0900

    Create : base package structure with MVP

--- app/src/main/java/com/aone/boostcamp/connect/menurandomchoice/views/main/MainActivity.java
+++ app/src/main/java/com/aone/boostcamp/connect/menurandomchoice/views/main/MainActivity.java
@@ -1,10 +1,12 @@
-package com.aone.boostcamp.connect.menurandomchoice;
+package com.aone.boostcamp.connect.menurandomchoice.views.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.aone.boostcamp.connect.menurandomchoice.R;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
