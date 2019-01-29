b4997adf12c18553d4590eb1e00bde92dffbb850
Ree <reemarkyll@gmail.com>
2019-01-29 22:15:04 +0900

    ADD : add button listener
    start button listener and upload button listener
    applying xml file naming convention

--- app/src/main/java/com/aone/boostcamp/connect/menurandomchoice/MainActivity.java
+++ app/src/main/java/com/aone/boostcamp/connect/menurandomchoice/MainActivity.java
@@ -2,12 +2,39 @@ package com.aone.boostcamp.connect.menurandomchoice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(this);
+
+        Button btnUpload = findViewById(R.id.btn_upload);
+        btnUpload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.btn_start:
+                //Move to Random Choice Activity
+                break;
+
+            case R.id.btn_upload:
+                //Move to Upload Store Information Activity
+                break;
+
+            default:
+                return;
+        }
     }
 }
