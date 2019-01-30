6ee3530df7c57dac5286150b8093a5642f7aa04f
ch-Yoon <chanhyuck1021@naver.com>
2019-01-30 11:01:19 +0900

    Create : keyboard and network util

---
+++ app/src/main/java/com/aone/menurandomchoice/utils/KeyboardUtil.java
@@ -0,0 +1,21 @@
+package com.aone.menurandomchoice.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeyboardUtil {
+
+    public static void hideKeyboard(@NonNull Context context) {
+        View view = ((Activity)context).getCurrentFocus();
+        if(view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(inputMethodManager != null) {
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+    }
+
+}
