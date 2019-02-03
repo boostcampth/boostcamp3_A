17726bbbbcd49f53c146d7bad8a5db9b5baa3c40
Ree <reemarkyll@gmail.com>
2019-02-03 17:24:10 +0900

    PULL : feature/OL02
    commit my local develop before pulling origin/develop

--- app/src/main/java/com/aone/menurandomchoice/data/Repository.java
+++ app/src/main/java/com/aone/menurandomchoice/data/Repository.java
@@ -2,7 +2,7 @@ package com.aone.menurandomchoice.data;
 
 import com.aone.menurandomchoice.data.oauth.KakaoLoginHelper;
 
-public interface Repository extends KakaoLoginHelper {
+public interface Repository extends KakaoLoginHelper, {
 
     void cancelAll();
 
