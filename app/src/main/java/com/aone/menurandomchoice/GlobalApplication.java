04b35da7db8dc39e8420247077c02f8ba3669618
ch-Yoon <chanhyuck1021@naver.com>
2019-02-06 22:35:21 +0900

    apply multidex

--- app/src/main/java/com/aone/menurandomchoice/GlobalApplication.java
+++ app/src/main/java/com/aone/menurandomchoice/GlobalApplication.java
@@ -1,11 +1,13 @@
 package com.aone.menurandomchoice;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.aone.menurandomchoice.repository.oauth.KakaoSDKAdapter;
 import com.kakao.auth.KakaoSDK;
 
-// FIXME 사용하는 라이브러리가 많아지면 나중에 multidex환경을 구성해야 하므로 MultiDexApplicatoin 사용을 고려해주세요
+import androidx.multidex.MultiDex;
+
 public class GlobalApplication extends Application {
     private static volatile GlobalApplication instance = null;
 
@@ -26,6 +28,11 @@ public class GlobalApplication extends Application {
     }
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    @Override
     public void onTerminate() {
         super.onTerminate();
 
@@ -35,5 +42,6 @@ public class GlobalApplication extends Application {
     private void setUp() {
         instance = this;
         KakaoSDK.init(new KakaoSDKAdapter());
+        MultiDex.install(this);
     }
 }
