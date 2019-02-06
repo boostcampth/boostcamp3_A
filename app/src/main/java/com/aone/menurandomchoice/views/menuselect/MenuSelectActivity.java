e4e6ad1b89106b1e04ddfb795d5c454763afef44
ch-Yoon <chanhyuck1021@naver.com>
2019-02-06 23:48:03 +0900

    MS01 - create basic MenuSelectActivity

---
+++ app/src/main/java/com/aone/menurandomchoice/views/menuselect/MenuSelectActivity.java
@@ -0,0 +1,16 @@
+package com.aone.menurandomchoice.views.menuselect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aone.menurandomchoice.R;
+
+public class MenuSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_select);
+    }
+}
