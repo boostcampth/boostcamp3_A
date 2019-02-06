d96e3005d89d7471cf55c4bdca5ae52d24f86d70
tedpark <gun0912@naver.com>
2019-02-06 14:15:21 +0900

    review-190206 Application클래스 리뷰

--- app/src/main/java/com/aone/menurandomchoice/GlobalApplication.java
+++ app/src/main/java/com/aone/menurandomchoice/GlobalApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.aone.menurandomchoice.repository.oauth.KakaoSDKAdapter;
 import com.kakao.auth.KakaoSDK;
 
+// FIXME 사용하는 라이브러리가 많아지면 나중에 multidex환경을 구성해야 하므로 MultiDexApplicatoin 사용을 고려해주세요
 public class GlobalApplication extends Application {
     private static volatile GlobalApplication instance = null;
 
