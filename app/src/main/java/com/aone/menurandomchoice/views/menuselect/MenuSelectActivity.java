eda6dd135ad997a468584f5022f621ec15176551
ch-Yoon <chanhyuck1021@naver.com>
2019-02-11 20:21:58 +0900

    MS02 - create menu select item view xml

--- app/src/main/java/com/aone/menurandomchoice/views/menuselect/MenuSelectActivity.java
+++ app/src/main/java/com/aone/menurandomchoice/views/menuselect/MenuSelectActivity.java
@@ -37,11 +37,26 @@ public class MenuSelectActivity extends AppCompatActivity {
 
     private List<TestData> getTestDataList() {
         List<TestData> testDataList = new ArrayList<>();
-        testDataList.add(new TestData(R.drawable.test1));
-        testDataList.add(new TestData(R.drawable.test2));
-        testDataList.add(new TestData(R.drawable.test3));
-        testDataList.add(new TestData(R.drawable.test4));
-        testDataList.add(new TestData(R.drawable.test5));
+        testDataList.add(new TestData(R.drawable.test1, "김치말이국수",
+                "이거 완전맛있어용~!!! 먹어보세용~!!!! 두말하면 잔소리~!!",
+                "15,000",
+                "한식"));
+        testDataList.add(new TestData(R.drawable.test2, "얼큰순대국",
+                "숙취 해소에는 이만한게 없지용~!!",
+                "8,000",
+                "양식"));
+        testDataList.add(new TestData(R.drawable.test3, "이름이 엄청나게 긴 메뉴입니당",
+                "메뉴 설명 짧게도 해보고",
+                "15,000",
+                "중식"));
+        testDataList.add(new TestData(R.drawable.test4, "라면",
+                "이건 엄청나게 기니까 메뉴 설명이 ...으로 표시되지 않을까 싶은데요 과연 어디까지 표현이 될까요",
+                "15,000,000",
+                "한식"));
+        testDataList.add(new TestData(R.drawable.test5, "얼쑤",
+                "딩딩딩 디기디기디기디 딩딩!",
+                "8,000",
+                "한식"));
 
         return testDataList;
     }
