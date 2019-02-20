49d74512b73ebb747b38a6609076b4fa67c0664a
ch-Yoon <chanhyuck1021@naver.com>
2019-02-20 18:45:17 +0900

    resolve owner store screen error

--- app/src/main/java/com/aone/menurandomchoice/utils/BindingUtil.java
+++ app/src/main/java/com/aone/menurandomchoice/utils/BindingUtil.java
@@ -40,6 +40,6 @@ public class BindingUtil {
             closetime = " ";
         }
 
-        textView.setText(opentime + "-" + closetime);
+        textView.setText(opentime + " - " + closetime);
     }
 }
